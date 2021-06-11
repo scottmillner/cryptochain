4aee69331f2da355ed6b0867e752421874d1373b
Scott Millner <scott@welldatalabs.com>
2021-06-11 08:19:25 -0600

    and endpoint to create a new block

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 
 const app = express();
+app.use(bodyParser.json());
 const blockchain = new Blockchain();
 
 app.get('/api/blocks', (req, res) => {
 	res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+	const { data } = req.body;
+
+	blockchain.addBlock(data);
+
+	res.redirect('/api/blocks');
 });
 
 const PORT = 3000;
