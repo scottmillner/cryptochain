98013f7433959f00242a917da3a81a0fd2797399
Scott Millner <scott@welldatalabs.com>
2021-06-14 13:06:48 -0600

    broadcast to network when new block added, allow new node peers to dynamically set their port

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
+const PubSub = require('./pubsub');
 
 const app = express();
 app.use(bodyParser.json());
 const blockchain = new Blockchain();
+const pubsub = new PubSub(blockchain);
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.get('/api/blocks', (req, res) => {
 	res.json(blockchain.chain);
@@ -14,9 +18,17 @@ app.post('/api/mine', (req, res) => {
 	const { data } = req.body;
 
 	blockchain.addBlock(data);
+	pubsub.broadcastChain();
 
 	res.redirect('/api/blocks');
 });
 
-const PORT = 3000;
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+	PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => console.log(`listening at localhost:${PORT}`));
