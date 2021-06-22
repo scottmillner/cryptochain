7c3c3a40e4b0cdb516cc9c18a12e26e958bf539a
Scott Millner <scott@welldatalabs.com>
2021-06-22 07:16:45 -0600

    add Transaction.validTransaction() and tests

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid');
+const { verifySignature } = require('../util');
 
 class Transaction {
 	constructor(senderWallet, recepient, amount) {
@@ -22,6 +23,26 @@ class Transaction {
 			address: senderWallet.publicKey,
 			signature: senderWallet.sign(outputMap),
 		};
+	}
+
+	static validTransaction(transaction) {
+		const {
+			input: { amount, address, signature },
+			outputMap,
+		} = transaction;
+
+		const outputTotal = Object.values(outputMap).reduce((total, outputAmount) => total + outputAmount);
+		if (amount !== outputTotal) {
+			console.error(`Invalid transaction from: ${address}`);
+			return false;
+		}
+
+		if (!verifySignature(address, outputMap, signature)) {
+			console.error(`Invalid signature from: ${address}`);
+			return false;
+		}
+
+		return true;
 	}
 }
 
--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -33,31 +33,71 @@ describe('Transaction', () => {
 			// Assert
 			expect(transaction.outputMap[senderWallet.publicKey]).toEqual(senderWallet.balance - amount);
 		});
+	});
 
-		describe('input', () => {
-			it('has an `input`', () => {
-				// Assert
-				expect(transaction).toHaveProperty('input');
-			});
+	describe('input', () => {
+		it('has an `input`', () => {
+			// Assert
+			expect(transaction).toHaveProperty('input');
+		});
 
-			it('has a `timestamp` in the input', () => {
-				// Assert
-				expect(transaction.input).toHaveProperty('timestamp');
-			});
+		it('has a `timestamp` in the input', () => {
+			// Assert
+			expect(transaction.input).toHaveProperty('timestamp');
+		});
 
-			it('sets the `amount` to the `senderWallet` balance', () => {
-				// Assert
-				expect(transaction.input.amount).toEqual(senderWallet.balance);
-			});
+		it('sets the `amount` to the `senderWallet` balance', () => {
+			// Assert
+			expect(transaction.input.amount).toEqual(senderWallet.balance);
+		});
 
-			it('sets the `address` to the `senderWallet` publicKey', () => {
-				// Assert
-				expect(transaction.input.address).toEqual(senderWallet.publicKey);
-			});
+		it('sets the `address` to the `senderWallet` publicKey', () => {
+			// Assert
+			expect(transaction.input.address).toEqual(senderWallet.publicKey);
+		});
 
-			it('signs the input', () => {
+		it('signs the input', () => {
+			// Act/Assert
+			expect(verifySignature(senderWallet.publicKey, transaction.outputMap, transaction.input.signature)).toBe(true);
+		});
+	});
+
+	describe('validTransaction()', () => {
+		let errorMock;
+
+		beforeEach(() => {
+			errorMock = jest.fn();
+			global.console.error = errorMock;
+		});
+
+		describe('when the transaction is valid', () => {
+			it('returns true', () => {
 				// Act/Assert
-				expect(verifySignature(senderWallet.publicKey, transaction.outputMap, transaction.input.signature)).toBe(true);
+				expect(Transaction.validTransaction(transaction)).toBe(true);
+			});
+		});
+
+		describe('when the transaction in invalid', () => {
+			describe('and a transaction outputMap value is invalid', () => {
+				it('returns false and logs an error', () => {
+					// Arrange
+					transaction.outputMap[senderWallet.publicKey] = 999999;
+
+					// Act/Assert
+					expect(Transaction.validTransaction(transaction)).toBe(false);
+					expect(errorMock).toHaveBeenCalled();
+				});
+			});
+
+			describe('and the transaction input signature is invalid', () => {
+				it('returns false and logs an error', () => {
+					// Arrange
+					transaction.input.signature = new Wallet().sign('data');
+
+					// Act/Assert
+					expect(Transaction.validTransaction(transaction)).toBe(false);
+					expect(errorMock).toHaveBeenCalled();
+				});
 			});
 		});
 	});
