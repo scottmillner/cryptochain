c8d4992806b533e9aa0c3fae10f44f34c7570ade
Scott Millner <scott@welldatalabs.com>
2021-06-28 07:33:47 -0600

    add endpoint to post transaction

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const bodyParser = require('body-parser');
 const axios = require('axios').default;
 const Blockchain = require('./blockchain');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet');
 
 const app = express();
 app.use(bodyParser.json());
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub(blockchain);
 
 const DEFAULT_PORT = 3000;
@@ -16,13 +20,21 @@ app.get('/api/blocks', (req, res) => {
 	res.json(blockchain.chain);
 });
 
-app.post('/api/mine', (req, res) => {
+app.post('/api/block', (req, res) => {
 	const { data } = req.body;
 
 	blockchain.addBlock(data);
 	pubsub.broadcastChain();
 
 	res.redirect('/api/blocks');
+});
+
+app.post('/api/transaction', (req, res) => {
+	const { amount, recipient } = req.body;
+	const transaction = wallet.createTransaction(recipient, amount);
+	transactionPool.setTransaction(transaction);
+
+	res.json(transaction);
 });
 
 const syncChains = () => {
